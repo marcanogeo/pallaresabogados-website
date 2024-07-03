e1bcc0c6ca1ffdf7aa8f5a052afd15bb1ab64ff9
georgelysmarcano <geogemar@hotmail.com>
2024-07-03 21:31:26 +0200

    update tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens:{
+			sm: '480px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1440px',
+			xxl: '1920px'
+		},
+		colors: {
+			'blue': '#1fb6ff',
+			'purple': '#7e5bef',
+			'pink': '#ff49db',
+			'orange': '#ff7849',
+			'green': '#13ce66',
+			'yellow': '#ffc82c',
+			'gray-dark': '#273444',
+			'gray': '#8492a6',
+			'gray-light': '#d3dce6',
+		},
+		fontFamily: {
+			sans: ['Graphik', 'sans-serif'],
+			serif: ['Lato', 'serif'],
+		},
 		extend: {},
 	},
 	plugins: [],
