c300a210a75071f17589c6f2e67843b4fbaf91eb
georgelysmarcano <geogemar@hotmail.com>
2024-08-02 21:55:46 +0200

    update validateform

--- src/validateform.js
+++ src/validateform.js
@@ -1,38 +1,24 @@
-const email = document.getElementsByName('email');
-const nameinput = document.getElementsByName('name');
-const message = document.getElementsByName('message');
-const subject = document.getElementsByName('subject');
-const error = document.getElementsByClassName('error');
-const $form = document.getElementById('form');
-$form.addEventListener('submit', function (e){
+const email = document.querySelector('[name=email]');
+const username = document.querySelector('[name=full_name]');
+const message = document.querySelector('[name=message]');
+const subject = document.querySelector('[name=subject]');
+const inputerror = document.querySelector('[class=error]');
+//const $form = document.getElementById('form');
+function validateinputs(e, message){
     e.preventDefault();
-    if(nameinput.value === '' || nameinput.value === null){
-        nameinput.classList.add('invalid');
-        nameinput.nextElementSibling.innerText = "Por favor ingrese su nombre y apellido";
+    const field = e.target;
+    const inputvalue = e.target.value;
+    if(inputvalue.length === 0){
+        inputvalue.classList.add('invalid');
+        inputerror.innerText = message;
+       // inputerror.innerText = `${field.name} es requerido.`;
     }else{
-        nameinput.classList.remove('invalid');
-        nameinput.nextElementSibling.innerText = "";
-    }
-    if(email.value === '' || email.value === null){
-        nameinput.classList.add('invalid');
-        nameinput.nextElementSibling.innerText = 'Por favor ingrese su e-mail';
-    }else{
-        nameinput.classList.remove('invalid');
-        nameinput.nextElementSibling.innerText = "";
-    }
-    if(subject.value === '' || subject.value === null){
-        nameinput.classList.add('invalid');
-        nameinput.nextElementSibling.innerText = 'Por favor indique el asunto o tema';
-    }else{
-        nameinput.classList.remove('invalid');
-        nameinput.nextElementSibling.innerText = "";
-    }
-    if(message.value === '' || message.value === null){
-        nameinput.classList.add('invalid');
-        nameinput.nextElementSibling.innerText = 'Por favor exprese brevemente su inquietud';
-    }else{
-        nameinput.classList.remove('invalid');
-        nameinput.nextElementSibling.innerText = "";
+        inputvalue.classList.remove('invalid');
+        inputerror.innerText = "";
     }
     return false;
-})
+}
+username.addEventListener('submit', (e) => validateinputs("Por favor, agregue su nombre completo"));
+email.addEventListener('submit', (e) => validateinputs ("Por favor, agregue su email"));
+message.addEventListener('submit', (e) => validateinputs("Por favor, describa brevemente su duda o en qué necesita asesoria"));
+subject.addEventListener('submit', (e) => validateinputs("Indique brevemente el tema a tratar"));
