c53268b1bf6b7371ec11cfc90f3e85bf8f13306e
georgelysmarcano <geogemar@hotmail.com>
2024-08-04 15:46:31 +0200

    update validate function at form

--- src/validateform.js
+++ src/validateform.js
@@ -1,29 +1,59 @@
-const email = document.querySelector('[name=email]');
-const username = document.querySelector('[name=full_name]');
-const message = document.querySelector('[name=message]');
-const subject = document.querySelector('[name=subject]');
-const inputerror = document.querySelector('[class=error]');
-//const $form = document.getElementById('form');
-function validateinputs(e, message){
-    e.preventDefault();
-    const field = e.target;
-    const inputvalue = e.target.value;
-    const emailregex = /^[a-z0-9]+@[a-z0-9]+\.[a-z]{2,3}$/;
-    console.log(emailregex.test(field.value));
-   // const messageregex = /^[a-z0-9]+@[a-z]{2,3}$/;
-   // const usernameregex = /^[a-z0-9]+@[a-z]{2,3}$/;
-   // const subjectregex = /^[a-z0-9]+@[a-z]{2,3}$/;
-    if(inputvalue.length === 0){
-        inputvalue.classList.add('invalid');
-        inputerror.innerText = message;
-       // inputerror.innerText = `${field.name} es requerido.`;
-    }else{
-        inputvalue.classList.remove('invalid');
-        inputerror.innerText = "";
+const userNameField = document.querySelector("[name=full_name]");
+//const passwordField = document.querySelector("[name=password]");
+const emailField = document.querySelector("[name=email]");
+const subjectField = document.querySelector("[name=subject]");
+const messageField = document.querySelector("[name=message]");
+//const fileField = document.querySelector("[name=avatar]");
+
+const setErrors = (message, field, isError = true) => {
+    if (isError) {
+        field.classList.add("invalid");
+        field.nextElementSibling.classList.add("error");
+        field.nextElementSibling.innerText = message;
+    } else {
+        field.classList.remove("invalid");
+        field.nextElementSibling.classList.remove("error");
+        field.nextElementSibling.innerText = "";
     }
-    return false;
 }
-username.addEventListener('submit', (e) => validateinputs("Por favor, agregue su nombre completo"));
-email.addEventListener('submit', (e) => validateinputs ("Por favor, agregue su email"));
-message.addEventListener('submit', (e) => validateinputs("Por favor, describa brevemente su duda o en qué necesita asesoria"));
-subject.addEventListener('submit', (e) => validateinputs("Indique brevemente el tema a tratar"));
+
+const validateEmptyField = (message, e) => {
+    const field = e.target;
+    const fieldValue = e.target.value;
+    if (fieldValue.trim().length === 0) {
+        setErrors(message, field);
+    } else {
+        setErrors("", field, false);
+    }
+}
+//const emailRegexp = /^[a-z0-9]+@[a-z0-9]+\.[a-z]{2,3}$/;
+const validateEmailFormat = e => {
+    const field = e.target;
+    const fieldValue = e.target.value;
+    const regex = new RegExp(/^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/);
+   // const regex = new RegExp(/^[a-z0-9]+@[a-z0-9]+\.[a-z]{2,3}$/);
+    if (fieldValue.trim().length > 5 && !regex.test(fieldValue)) {
+        setErrors("Por favor, ingrese un email válido", field);
+    } else {
+        setErrors("", field, false);
+    }
+}
+
+userNameField.addEventListener("blur", (e) => validateEmptyField("Por favor, ingrese su nombre completo", e));
+subjectField.addEventListener("blur", (e) => validateEmptyField("Indique el tema en el que necesita asesoria de forma breve", e));
+//emailField.addEventListener("blur", (e) => validateEmptyField("Por favor, ingrese su email", e));
+messageField.addEventListener("blur", (e) => validateEmptyField("Explique brevemente la situación o duda a tratar", e));
+
+emailField.addEventListener("input", validateEmailFormat);
+
+/*
+fileField.addEventListener("change", (e) => {
+    const field = e.target;
+    const fileExt = e.target.files[0].name.split(".").pop().toLowerCase();
+    const allowedExt = ["jpg", "jpeg", "png", "gif"];
+    if (!allowedExt.includes(fileExt)) {
+        setErrors(`The only extensions allowed are ${allowedExt.join(", ")}`, field);
+    } else {
+        setErrors("", field, false);
+    }
+});*/
